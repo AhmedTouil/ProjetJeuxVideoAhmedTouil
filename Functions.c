c996bd8cba0ff67d85d77214bf7684368ae0fd84
AhmedTouil <47881717+AhmedTouil@users.noreply.github.com>
2019-05-04 13:28:47 -0700

    Update Functions.c

--- Functions.c
+++ Functions.c
@@ -4,7 +4,9 @@
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_mixer.h>
 #include "test.h"
-
+int sigh=0;
+int dist=0;
+int state=0;
 int FPS = 50;    
 int NextTick , interval ;
 void FPS_Initial(void) {
@@ -63,33 +65,116 @@ SDL_SetColorKey(pic,SDL_SRCCOLORKEY|SDL_RLEACCEL,SDL_MapRGB(pic->format,R,G,B));
 return (pic);
 }
 
-void updateScreenPlayer(int num)
+void updateScreenPlayer(int num,character chara1)
 {
 SDL_Rect animRect ;
-animRect.x=chara.x; 
-animRect.y=chara.y;
+animRect.x=chara1.x; 
+animRect.y=chara1.y;
 SDL_FillRect(scr.ecran,NULL,0x221122);
-SDL_BlitSurface(chara.anim[num], NULL , scr.ecran , &animRect );
-	SDL_Flip(scr.ecran);
+SDL_BlitSurface(chara1.anim[num], NULL , scr.ecran , &animRect );	
 }
-
+void updateScreenEnnemy(int num,oponent chara1)
+{
+SDL_Rect enemiRect;
+enemiRect.x=chara1.x; 
+enemiRect.y=chara1.y;
+SDL_BlitSurface(chara1.enemi[num], NULL , scr.ecran , &enemiRect );
+SDL_Flip(scr.ecran);	
+}
 int animateplayer(int aTick,int num)
 {
 if( aTick%5 == 0 ) num++;
 if(num >= justice_No) num = 0;
 return num;
 }
-
+int animateEnnemys(int aTick,int num)
+{
+if( aTick%5 == 0 ) num++;
+if(num >= Ennemy_No) num = 0;
+return num;
+}
 void animateEnnemy(void)
 {
-if(chara.x==250)
+if(Ennemy.x==250)
 {
+Ennemy.dir=-1;
+}
+if (Ennemy.x==180)
+{
+Ennemy.dir=1;
+}
+Ennemy.x=Ennemy.x+2*Ennemy.dir;
+}
+
+int ai(void)
+{
+
+if(chara.dir!=Ennemy.dir) 
+{
+if(chara.x<Ennemy.x)
+{
+sigh=-1;
 chara.dir=-1;
 }
-if (chara.x==0)
+if(chara.x>Ennemy.x)
 {
+sigh=1;
 chara.dir=1;
 }
-chara.x=chara.x+2*chara.dir;
 }
+
+if(chara.dir==Ennemy.dir)
+{
+if(chara.x>Ennemy.x)
+{
+sigh=1;
+chara.dir=1;
+}
+if(chara.x<Ennemy.x)
+{
+sigh=-1;
+chara.dir=-1;
+}
+}
+
+dist=sigh*(chara.x-Ennemy.x);
+if ((dist>=20)&&(dist<=60))
+{
+state=1;
+}
+else if (dist>50) 
+{
+state=2;
+}
+else if(dist<20)
+{
+state=3;
+}
+switch (state)
+{
+case 1 :
+Ennemy.x+=2*Ennemy.dir;
+break;
+case 2 :
+if ((Ennemy.initx<Ennemy.x) && (Ennemy.x>Ennemy.initx+90))   
+{
+Ennemy.dir=-1;
+}
+if ((Ennemy.initx>Ennemy.x) && (Ennemy.x<Ennemy.initx-20))
+{
+Ennemy.dir=1;
+}
+Ennemy.x=Ennemy.x+2*Ennemy.dir;
+
+break;
+
+case 3 :
+Ennemy.dir=chara.dir;
+break;
+}
+return(state);
+}
+
+
+
 
